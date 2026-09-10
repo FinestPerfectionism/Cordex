3abb7f9881f12f06b0ea7bedc193bfc731b33c44
Nick Engriss <nickengrissix@gmail.com>
2026-09-10 22:13:08 +0000

    remove fancy formatting in logging

--- main.py
+++ main.py
@@ -1,50 +1,12 @@
 import sys
 from asyncio import run
-from logging import (
-    CRITICAL,
-    DEBUG,
-    ERROR,
-    INFO,
-    WARNING,
-    Formatter,
-    LogRecord,
-    StreamHandler,
-)
-from logging import getLogger as get_logger
+from logging import INFO
+from logging import basicConfig as basic_config
 from os import getenv
-from typing import ClassVar, final, override
 
 from dotenv import load_dotenv
 
 from bot import bot, log
-from constants import COLOR_BLACK, COLOR_BLUE, COLOR_GREY, COLOR_RED, COLOR_YELLOW
-
-
-def _hex_to_ansi(hex_value : int) -> str:
-    r = (hex_value >> 16) & 0xFF
-    g = (hex_value >> 8)  & 0xFF
-    b = hex_value         & 0xFF
-    return f"\x1b[38;2;{r};{g};{b}m"
-
-@final
-class CustomFormatter(Formatter):
-    reset   = "\x1b[0m"
-    log_fmt = "%(asctime)s | {}%(levelname)s{} | %(name)s | %(message)s"
-
-    FORMATS : ClassVar = {
-        DEBUG    : log_fmt.format(_hex_to_ansi(COLOR_GREY.value),   reset),
-        INFO     : log_fmt.format(_hex_to_ansi(COLOR_BLUE.value),   reset),
-        WARNING  : log_fmt.format(_hex_to_ansi(COLOR_YELLOW.value), reset),
-        ERROR    : log_fmt.format(_hex_to_ansi(COLOR_RED.value),    reset),
-        CRITICAL : log_fmt.format(_hex_to_ansi(COLOR_BLACK.value),  reset),
-    }
-
-    @override
-    def format(self, record : LogRecord) -> str:
-        log_fmt   = self.FORMATS.get(record.levelno, self.log_fmt)
-        formatter = Formatter(log_fmt)
-        return formatter.format(record)
-
 
 # ⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻
 # Main Script
@@ -52,16 +14,14 @@ class CustomFormatter(Formatter):
 
 load_dotenv()
 
-stream_handler = StreamHandler()
-stream_handler.setFormatter(CustomFormatter())
-
-root_logger = get_logger()
-root_logger.setLevel(INFO)
-root_logger.addHandler(stream_handler)
+basic_config(
+    level  = INFO,
+    format = "%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+)
 
 TOKEN = getenv("TOKEN")
 
-async def main() -> None:
+async def _main() -> None:
     if not TOKEN:
         error = "TOKEN environment variable not set."
         raise RuntimeError(error)
@@ -77,7 +37,7 @@ async def main() -> None:
 
 if __name__ == "__main__":
     try:
-        run(main())
+        run(_main())
     except KeyboardInterrupt:
         log.info("Received error — KeyboardInterrupt")
     except Exception:
