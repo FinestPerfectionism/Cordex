81ad60303ed4bf10b30776fe156281d793323657
Nick Engriss <nickengrissix@gmail.com>
2026-09-12 23:27:19 +0000

    format

--- commands/bot_owner/eval/cmd.py
+++ commands/bot_owner/eval/cmd.py
@@ -85,7 +85,7 @@ async def run_bo_eval(ctx : Context, body : str) -> None:
         "ui"           : ui,
 
         "ACCEPTED_EMOJI"   : ACCEPTED_EMOJI,
-        "WARNING_EMOJI"  : WARNING_EMOJI,
+        "WARNING_EMOJI"    : WARNING_EMOJI,
         "DENIED_EMOJI"     : DENIED_EMOJI,
         "STANDSTILL_EMOJI" : STANDSTILL_EMOJI,
 
