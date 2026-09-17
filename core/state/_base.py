366934bf09cc166ec8ade36e47617b9aba3a034e
Nick Engriss <nickengrissix@gmail.com>
2026-09-17 20:59:55 +0000

    enforce t-string only in execute, misc

--- core/state/_base.py
+++ core/state/_base.py
@@ -1,4 +1,3 @@
-from collections.abc import Iterable
 from pathlib import Path
 from string.templatelib import Template
 from typing import cast, override
@@ -15,19 +14,13 @@ from aiosqlite.context import Result
 
 class Connection(AiosqliteConnection):
     @override
-    def execute(self, sql : str | Template, parameters : Iterable[object] | None = None) -> Result[Cursor]:
-        if isinstance(sql, Template):
-            if parameters is not None:
-                error = "Cannot pass extra parameters when using a t-string."
-                raise ValueError(error)
-
-            return super().execute("?".join(sql.strings), tuple(sql.values))
-        return super().execute(sql, parameters)
+    def execute(self, sql : Template, /) -> Result[Cursor]:
+        return super().execute("?".join(sql.strings), sql.values)
 
 
 async def connect(database : str | Path) -> Connection:
     raw_connection = await aiosqlite_connect(database)
 
-    conn = cast("Connection", raw_connection)
-    conn.__class__ = Connection
-    return conn
+    connection = cast("Connection", raw_connection)
+    connection.__class__ = Connection
+    return connection
