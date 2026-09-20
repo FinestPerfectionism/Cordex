2b54a655f159decbeeb5405791950751a75093c0
Nick Engriss <nickengrissix@gmail.com>
2026-09-20 18:57:45 +0000

    whoops

--- commands/bot_owner/__init__.py
+++ commands/bot_owner/__init__.py
@@ -1,18 +1,8 @@
-from .cogs import (
-    run_bo_cog_load,
-    run_bo_cog_pullreload,
-    run_bo_cog_reload,
-    run_bo_cog_unload,
-)
 from .eval import run_bo_eval
 from .state import run_bo_state_restart, run_bo_state_shutdown, run_bo_state_sync
 from .style import run_bo_style_reset, run_bo_style_set
 
 __all__ = [
-    "run_bo_cog_load",
-    "run_bo_cog_pullreload",
-    "run_bo_cog_reload",
-    "run_bo_cog_unload",
     "run_bo_eval",
     "run_bo_state_restart",
     "run_bo_state_shutdown",
